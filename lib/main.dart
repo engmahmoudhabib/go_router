f143ae25569a8d641a7c7b8218919f971832ac8a
Mahmoud_13 <mahmoudhabib.eng@gmail.com>
2023-10-03 03:01:10 +0000

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:routingone/pages/home.dart';
+import 'package:routingone/pages/profile.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,35 +10,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+    return MaterialApp.router(
+      routerConfig: GoRouter(
+        routes: [
+          GoRoute(
+            path: '/',
+            builder: (context, state) => const Home(),
+          ),
+          GoRoute(
+            path: '/profile',
+            builder: (context, state) => const Profile(),
+          )
+        ],
       ),
     );
   }
---
+++ lib/pages/profile.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: const Center(
+        child: Text(
+          'Hello, World!',
+        ),
+      ),
+    );
+  }
+}
